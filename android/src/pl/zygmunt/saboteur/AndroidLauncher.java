fd02409585c9492ec1815953bf99abe85b4dac82
piozygmunt <piotrek55x@gmail.com>
2016-06-20 20:52:45 +0200

    new icon and android config changes

--- android/src/pl/zygmunt/saboteur/AndroidLauncher.java
+++ android/src/pl/zygmunt/saboteur/AndroidLauncher.java
@@ -11,6 +11,12 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		
+		config.disableAudio = true;
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useGyroscope = false;
+		
 		initialize(new Saboteur(), config);
 	}
 }
